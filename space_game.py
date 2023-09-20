69d0297eeba31b8ccab40990b56276c84438582c
Stef Buurman <St3fbuurman@gmail.com>
2023-09-20 12:08:53 +0200

    simpele intro

---
+++ space_game.py
@@ -0,0 +1,10 @@
+def intro():
+    print("Welkom bij het o zo mooie SpacerGame")
+    player_name = input("Vul hier de naam van je character in: ")
+    print(f"\nWelkom {player_name}, jij bent een van de astronauten in een operatie om de ruimte te onderzoeken.")
+    print("Jou doel is om zo veel mogelijk kennis te vergaren van de ruimte.")
+    return player_name
+
+
+
+player_name = intro()
