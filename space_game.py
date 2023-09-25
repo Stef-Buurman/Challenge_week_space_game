ba096a3a2b4e5910829c980bb11143126e29a15c
Stef Buurman <St3fbuurman@gmail.com>
2023-09-25 14:31:33 +0200

    Clear console and start adventure function

--- space_game.py
+++ space_game.py
@@ -1,9 +1,16 @@
 from sound import Sound
 from combat import Combat
 from spaceship import Spaceship
+from planet import Planet
 import time
+import os
+
 
 def intro(background):
+    if os.name == 'posix':
+        os.system('clear')
+    elif os.name == 'nt':
+        os.system('cls')
     # background.play()
     print("Welcome to the beautiful SpacerGame")
     player_name = input("Enter the name of you character: ")
@@ -19,10 +26,17 @@ def intro(background):
     return spaceship
 
 def startAdventure(spaceship):
-    combat = Combat()
-    combat.inCombatPlanet(spaceship)
-
-        
+    print("\nTeam: aircraft is ready to launch.")
+    time.sleep(0.5)
+    print("Let's get to business!")
+    for i in sorted(list(range(1, 11)), reverse=True):
+        print(i)
+        time.sleep(1)
+    print("LAUNCH!!!")
+    Sound("Media\Launch.mp3", 5, False).play()
+    time.sleep(5)
+    planet = Planet()
+    planet.select_planet()
 
 background = Sound("Media\Background.mp3", 0, True)
 spaceship = intro(background)
