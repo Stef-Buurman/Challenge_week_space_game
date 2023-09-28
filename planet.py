1900a15f19e863937d0c304e342f6d4b7ac5391f
NickWijmans <nickwijmans@gmail.com>
2023-09-28 19:30:17 +0200

    Small fix

--- planet.py
+++ planet.py
@@ -117,7 +117,7 @@ class Planet:
 
         abc = ["A","B","C"]
 
-        input_string = "Choose your planet to visit:"
+        input_string = "\nChoose your planet to visit:"
             
         for i, planet in enumerate(three_random_planets):
             input_string += f"\n{abc[i]}: {planet['name']}: {planet['description']}"
